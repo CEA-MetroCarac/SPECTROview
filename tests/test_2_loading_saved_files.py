2a730296d8db0a823256058debd52c6773112820
h0an.le <vanhoan.le@outlook.com>
2024-09-26 18:13:39 +0200

    update test.py

--- tests/test_2_loading_saved_files.py
+++ tests/test_2_loading_saved_files.py
@@ -1,15 +1,10 @@
-# import sys
-# from pathlib import Path
-
-# sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
-
-# import pytest
-# from examples.ex_loading_saved_files import ex_loading_saved_files
-
-# def test_loading_saved_files(qtbot):
-#     ex_loading_saved_files(run_app=False) 
-
 import sys
 from pathlib import Path
 
-print("test")
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+import pytest
+from examples.ex_loading_saved_files import ex_loading_saved_files
+
+def test_2_loading_saved_files(qtbot):
+    ex_loading_saved_files(run_app=False) 

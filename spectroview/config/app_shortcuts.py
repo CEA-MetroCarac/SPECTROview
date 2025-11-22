f44f5e10a38a23c8b308253fdd54116baac09bf8
HoanLe <vanhoan.le@outlook.com>
2025-11-22 19:52:11 +0100

    fix bug related to app's shortcuts

--- spectroview/config/app_shortcuts.py
+++ spectroview/config/app_shortcuts.py
@@ -9,9 +9,9 @@ def rescale_shortcut(main_app):
     """Dispatch Ctrl+R based on current tab."""
     current_tab = main_app.ui.tabWidget.currentWidget()
     if current_tab == main_app.ui.tab_spectra:
-        main_app.spectrums.spectra_widget.rescale()
+        main_app.spectrums.spectra_viewer.rescale()
     elif current_tab == main_app.ui.tab_maps:
-        main_app.maps.spectra_widget.rescale()
+        main_app.maps.spectra_viewer.rescale()
 
 def fitting_shortcut(main_app):
     """Dispatch Ctrl+F based on current tab."""
@@ -21,15 +21,16 @@ def fitting_shortcut(main_app):
     elif current_tab == main_app.ui.tab_maps:
         main_app.maps.fit()
 
-def copy_shortcut(main_app):
-    """Dispatch Ctrl+F based on current tab."""
-    current_tab = main_app.ui.tabWidget.currentWidget()
-    if current_tab == main_app.ui.tab_spectra:
-        main_app.spectrums.spectra_widget.copy_fig()
-    elif current_tab == main_app.ui.tab_maps:
-        main_app.maps.spectra_widget.copy_fig()
-    elif current_tab == main_app.ui.tab_graphs:
-        main_app.visu.copy_fig_to_clb()
+# def copy_shortcut(main_app):
+#     """Dispatch Ctrl+C based on current tab."""
+#     current_tab = main_app.ui.tabWidget.currentWidget()
+#     if current_tab == main_app.ui.tab_graphs:
+#         main_app.visu.copy_fig_to_clb()
+
+#     elif current_tab == main_app.ui.tab_maps:
+#         main_app.maps.spectra_widget.copy_fig()
+#     elif current_tab == main_app.ui.tab_spectra:
+#         main_app.spectrums.spectra_widget.copy_fig()
 
 def setup_shortcuts(main_app):
     """
@@ -70,12 +71,12 @@ def setup_shortcuts(main_app):
 
 
     # FITTING action (Ctrl+F)
-    shortcut_rescale = QShortcut(QKeySequence(Qt.ControlModifier | Qt.Key_F), main_app.ui)
-    shortcut_rescale.setContext(Qt.ShortcutContext.ApplicationShortcut)
-    shortcut_rescale.activated.connect(lambda: fitting_shortcut(main_app))
+    shortcut_fitting = QShortcut(QKeySequence(Qt.ControlModifier | Qt.Key_F), main_app.ui)
+    shortcut_fitting.setContext(Qt.ShortcutContext.ApplicationShortcut)
+    shortcut_fitting.activated.connect(lambda: fitting_shortcut(main_app))
 
 
-    # COPY action (Ctrl+C)
-    shortcut_rescale = QShortcut(QKeySequence(Qt.ControlModifier | Qt.Key_C), main_app.ui)
-    shortcut_rescale.setContext(Qt.ShortcutContext.ApplicationShortcut)
-    shortcut_rescale.activated.connect(lambda: copy_shortcut(main_app))
+    # # COPY action (Ctrl+C)
+    # shortcut_copy = QShortcut(QKeySequence(Qt.ControlModifier | Qt.Key_C), main_app.ui)
+    # shortcut_copy.setContext(Qt.ShortcutContext.ApplicationShortcut)
+    # shortcut_copy.activated.connect(lambda: copy_shortcut(main_app))

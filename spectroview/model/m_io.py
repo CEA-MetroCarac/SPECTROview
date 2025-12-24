35be801184c4b6e05f9c6cac4fcaa2491a2fac54
HoanLe <vanhoan.le@outlook.com>
2025-12-24 21:39:28 +0100

    update m_io.py

--- spectroview/model/m_io.py
+++ spectroview/model/m_io.py
@@ -34,3 +34,40 @@ def load_spectrum_file(path: Path) -> MSpectrum:
 def load_map_file(path: Path) -> MSpectrum:
     """Load TXT or CSV spectrum file."""
     pass
+
+
+def load_dataframe_file(path: Path) -> pd.DataFrame:
+    """Load DataFrame from EXCEL or CSV file."""
+    ext = path.suffix.lower()
+
+    if ext == ".txt":
+        df = pd.read_csv(path, header=None, skiprows=1, delimiter="\t")
+    elif ext == ".csv":
+        df = pd.read_csv(path)
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
+    return df
+
+def save_spectra_workspace(path: Path, spectra: list[MSpectrum]):
+    """Save the current spectrum workspace to a .spectra file."""
+    pass
+
+def load_spectra_workspace(path: Path) -> list[MSpectrum]:
+    """Load a .spectra file into a list of MSpectrum objects."""
+    pass
+
+def save_maps_workspace(path: Path, maps: list[MSpectrum]):
+    """Save the current maps workspace to a .maps file."""
+    pass   
+
+def load_maps_workspace(path: Path) -> list[MSpectrum]:
+    """Load a .maps file into a list of MSpectrum objects."""
+    pass   
+
+def save_graphs_workspace(path: Path, dataframes: list[pd.DataFrame]):
+    """Save the current graphs workspace to a .graphs file."""
+    pass   
+
+def load_graphs_workspace(path: Path) -> list[pd.DataFrame]:
+    """Load a .graphs file into a list of DataFrame objects."""
+    pass
